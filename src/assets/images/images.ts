da5bfbf8c7a3112dcb01ad48839462408d8daa83
RoberthLoorDev <dayvidlife@gmail.com>
2024-12-10 18:23:34 -0500

    add: finish header section

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -1,7 +1,13 @@
 import icon from './icon.png';
 import bigIcon from './big-icon.png';
+import youtubeIcon from './youtube-icon.png';
+import facebookIcon from './facebook-icon.png';
+import twitterIcon from './twitter-icon.png';
 
 export const images = {
      icon: icon,
      bigIcon,
+     youtubeIcon,
+     facebookIcon,
+     twitterIcon,
 };
--- src/sections/HeaderSection.tsx
+++ src/sections/HeaderSection.tsx
@@ -2,7 +2,7 @@ import { images } from '../assets/images/images';
 
 export default function HeaderSection() {
      return (
-          <div className="flex justify-between mt-[155px]">
+          <div className="flex justify-between mt-[155px] relative">
                <div>
                     <p className="uppercase text-[20px] font-medium text-[#ACACAC] ">
                          Acorta y comparte enlaces de forma rápida y sencilla.
@@ -24,10 +24,10 @@ export default function HeaderSection() {
                     </div>
 
                     {/* title input */}
-                    <div className="bg-[#232227] w-[367px] h-[47px] mt-[8px] rounded-full">
+                    <div className="bg-[#232227] w-[367px] h-[50px] mt-[8px] rounded-full">
                          <input
                               type="text"
-                              className="w-full h-full rounded-full bg-transparent px-[31px] placeholder:opacity-30 outline-0"
+                              className="w-full h-full rounded-full bg-transparent px-[31px] placeholder:opacity-30 outline-0 text-white"
                               placeholder="Título (opcional)"
                          />
                     </div>
@@ -35,6 +35,22 @@ export default function HeaderSection() {
                <div className="flex items-center">
                     <img className="w-auto h-[385.075px] " src={images.bigIcon} alt="Ícono de la página" />
                </div>
+
+               {/* images icons */}
+               <img className="absolute right-[300px] w-auto h-[58px]" src={images.facebookIcon} alt="Facebook" />
+               <img
+                    className="absolute top-[200px] right-[350px] w-auto h-[58px]"
+                    src={images.twitterIcon}
+                    alt="Facebook"
+               />
+               <img
+                    className="absolute top-[380px] right-[150px] w-auto h-[58px]"
+                    src={images.youtubeIcon}
+                    alt="Facebook"
+               />
+
+               {/* bottom square */}
+               <div className="absolute top-[-800px] left-[1000px] w-[1172px] h-[1323px] bg-[#29272E] rounded-[50px] rotate-[40deg] -z-30"></div>
           </div>
      );
 }
